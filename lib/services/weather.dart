43f72587c8e90d59ed5c1d198cb32fe42d2f068b
Aditya Gupta <53346087+xcyberpunkx0@users.noreply.github.com>
2020-10-29 11:22:35 +0530

    Use your own API key for weatherData :P

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -4,6 +4,7 @@ import 'package:Minimal_Weather/utilities/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_icons/weather_icons.dart';
+import 'package:Minimal_Weather/utilities/apikey.dart';
 
 class WeatherModel {
   Future<dynamic> getCityWeather(String cityName) async {
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const apiKey = '6595089ac7da380d99c4ecd3b365769e';
 const String url = 'https://api.openweathermap.org/data/2.5/weather';
 
 const knewlocstyle = TextStyle(
